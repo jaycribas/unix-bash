1123d42dc74e4d8216a19a2a33c7df4af7385d55
Sally Maki <sallymaki@gmail.com>
2017-03-13 17:58:47 -0700

    Day 1 work, exercises for command line chapters 1 & 2

---
+++ command-line/ex1-3-2.sh
@@ -0,0 +1,4 @@
+echo "hello"
+echo "world"
+echo -n "hello2"
+echo -n " world2"
---
+++ command-line/ex2-2.sh
@@ -0,0 +1,7 @@
+# What’s the command to list all the non-hidden files and directories that start with the letter “s”?
+# What is the command to list all the non-hidden files that contain the string “onnet”, long-form by reverse modification time? Hint: Use the wildcard operator at both the beginning and the end.
+# What is the command to list all files (including hidden ones) by reverse modification time, in long form?
+
+ls s*
+ls -rtl *onnet*
+ls -a -rtl
